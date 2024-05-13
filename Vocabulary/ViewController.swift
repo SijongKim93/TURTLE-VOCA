e3438cf7c5e33c491061a0db8736a2a6a1b7c58b
SijongKim93 <rlatlwhd456@gmail.com>
2024-05-13 14:06:13 +0900

    [Setting] Git Setting

---
+++ Vocabulary/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Vocabulary
+//
+//  Created by 김시종 on 5/13/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
