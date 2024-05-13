cc6fcb85a240768f6e07bed2095535cbb6786a52
SijongKim93 <rlatlwhd456@gmail.com>
2024-05-13 18:20:22 +0900

    [Design] Calender, TableView UI 구성

--- Vocabulary/Controller/CalenderViewController.swift
+++ Vocabulary/Controller/CalenderViewController.swift
@@ -6,24 +6,72 @@
 //
 
 import UIKit
+import SnapKit
 
 class CalenderViewController: UIViewController {
+    
+    var selectedDate: DateComponents? = nil
+    
+    let dateView: UICalendarView = {
+       var view = UICalendarView()
+        view.wantsDateDecorations = true
+        return view
+    }()
+    
+    let viewLine: UIView = {
+        var view = UIView()
+        view.backgroundColor = .systemGray5
+        return view
+    }()
 
+    let dayTableView = UITableView()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        setupUI()
+        view.backgroundColor = .white
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func setupUI() {
+        view.addSubview(dateView)
+        view.addSubview(viewLine)
+        view.addSubview(dayTableView)
+        
+        dayTableView.delegate = self
+        dayTableView.dataSource = self
+        dayTableView.register(CalenderTableViewCell.self, forCellReuseIdentifier: CalenderTableViewCell.identifier)
+        
+        dateView.snp.makeConstraints {
+            $0.top.equalTo(view.safeAreaLayoutGuide).offset(20)
+            $0.leading.trailing.equalTo(view.safeAreaLayoutGuide).inset(10)
+        }
+        
+        viewLine.snp.makeConstraints {
+            $0.top.equalTo(dateView.snp.bottom).offset(50)
+            $0.leading.trailing.equalTo(view.safeAreaLayoutGuide)
+            $0.height.equalTo(1)
+        }
+        
+        dayTableView.snp.makeConstraints {
+            $0.top.equalTo(viewLine.snp.bottom).offset(20)
+            $0.leading.trailing.bottom.equalTo(view.safeAreaLayoutGuide)
+        }
     }
-    */
+    
+}
 
+
+extension CalenderViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: CalenderTableViewCell.identifier, for: indexPath) as? CalenderTableViewCell else { fatalError("테이블 뷰 에러") }
+        
+        
+        return cell
+    }
+    
+    
 }
