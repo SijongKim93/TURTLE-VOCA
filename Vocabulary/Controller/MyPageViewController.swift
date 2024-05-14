7060e937fc3c3f0372bfb1d4e2b1951117d47863
SijongKim93 <rlatlwhd456@gmail.com>
2024-05-14 10:29:59 +0900

    [Design] 마이페이지 UI 시작

--- Vocabulary/Controller/MyPageViewController.swift
+++ Vocabulary/Controller/MyPageViewController.swift
@@ -6,12 +6,22 @@
 //
 
 import UIKit
+import SnapKit
 
 class MyPageViewController: UIViewController {
     
+    let profileImage: UIImageView = {
+        let image = UIImageView()
+        image.layer.cornerRadius = image.frame.size.width / 2
+        image.clipsToBounds = true
+        return image
+    }()
     
+    let mailLabel = LabelFactory().makeLabel(title: "이메일", size: 20, textAlignment: .left, isBold: false)
+    let subLabel = LabelFactory().makeLabel(title: "닉네임", size: 17, textAlignment: .left, isBold: false)
     
     override func viewDidLoad() {
         super.viewDidLoad()
     }
+    
 }
