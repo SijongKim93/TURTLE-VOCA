ade820a4e4227b2cbd434f93d1866464efe2bf54
Hanbit Kim <gksqlc6901@naver.com>
2024-05-23 10:28:17 +0900

    [Fix]: 수정 페이지에 텍스트 필드에 키보드 가리지않게 하는 기능 추가하기

--- Vocabulary/Controller/EditBookCaseViewController.swift
+++ Vocabulary/Controller/EditBookCaseViewController.swift
@@ -66,6 +66,37 @@ class EditBookCaseViewController: UIViewController, EditBookCaseBodyViewDelegate
         picker.delegate = self
         present(picker, animated: true, completion: nil)
     }
+    
+    //텍스트 필드 입력 시 키보드가 가리지 않게
+    func keyboardWillShow(_ sender: Notification) {
+        guard let userInfo = sender.userInfo,
+              let keyboardFrame = userInfo[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue,
+              let animationDuration = userInfo[UIResponder.keyboardAnimationDurationUserInfoKey] as? Double else { return }
+        
+        let keyboardHeight = keyboardFrame.cgRectValue.height
+        let activeField = UIResponder.findFirstResponder() as? UIView
+        
+        guard let activeField = activeField else { return }
+        
+        let fieldFrame = activeField.convert(activeField.bounds, to: view)
+        let fieldBottom = fieldFrame.origin.y + fieldFrame.size.height
+        
+        let overlap = fieldBottom - (view.frame.height - keyboardHeight) + 20
+        if overlap > 0 {
+            UIView.animate(withDuration: animationDuration) {
+                self.view.frame.origin.y = -overlap
+            }
+        }
+    }
+    
+    func keyboardWillHide(_ sender: Notification) {
+        guard let userInfo = sender.userInfo,
+              let animationDuration = userInfo[UIResponder.keyboardAnimationDurationUserInfoKey] as? Double else { return }
+        
+        UIView.animate(withDuration: animationDuration) {
+            self.view.frame.origin.y = 0
+        }
+    }
 }
 
 extension EditBookCaseViewController: PHPickerViewControllerDelegate {
